06ce6cc9d41db6a5968499989194a0864b3944ce
Ivan Lee <ilee.code@gmail.com>
2020-03-03 00:20:51 -0800

    Imported redux react-redux to index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     {
@@ -25,4 +27,9 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
 
   return selectedSong;
-}
+};
+
+export default combineReducers ({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
