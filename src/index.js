32db02abbd2ddd881c86c4e5fdce5cc24e6a7869
Ivan Lee <ilee.code@gmail.com>
2020-03-03 00:40:05 -0800

    Implement Song List

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { createStore } from 'redux';
 import App from './components/App';
 import reducers from './reducers'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={createStore(reducers)}><App /></Provider>,
+  document.querySelector('#root')
+  );
 
 
 
